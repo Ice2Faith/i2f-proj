66d4530dd9048ac48de2a7b380f710d2cea2f51b
Ice2Faith <Ugex_Savelar@163.com>
2023-08-17 23:55:45 +0800

    verify code upgrade

--- i2f-web/src/components/VerifyCode.vue
+++ i2f-web/src/components/VerifyCode.vue
@@ -6,15 +6,20 @@
         <span>{{request.question}}</span>
       </div>
       <div class="verifycode-img">
-        <img v-if="request.base64 && request.base64!=''" :src="request.base64" @click="refresh" />
+        <img v-if="request.base64 && request.base64!=''"
+             :src="request.base64"
+             @click="onClickImg($event)" />
         <div v-else style="width: 100%;height: 46px;color: red" @click="refresh">
           验证码加载失败了，点击此处重试
         </div>
       </div>
-      <div>
+      <div v-if="request.type==0">
         <span>答案：</span>
         <input type="text" v-model="result" class="verifycode-result" @keydown.enter="submit"/>
         <input type="button" @click="submit" value="提交" class="verifycode-submit"/>
+      </div>
+      <div v-else style="text-align: end">
+        <input type="button" @click="refresh" value="刷新" class="verifycode-submit"/>
       </div>
     </div>
   </div>
@@ -32,18 +37,27 @@ export default {
           base64: '',
           code: '',
           width: 320,
-          height: 320
+          height: 320,
+          type: 1,
+          count: 0
         }
       }
     }
   },
   data () {
     return {
-      result: ''
+      result: '',
+      points:[],
+      zoom: 'normal'
     }
   },
   mounted () {
     this.refresh()
+    this.zoom=document.body.style.zoom
+    document.body.style.zoom='normal'
+  },
+  unmounted() {
+    document.body.style.zoom=this.zoom
   },
   methods: {
     refresh () {
@@ -54,7 +68,51 @@ export default {
         code: this.request.code,
         result: this.result
       })
-    }
+    },
+    onClickImg(event){
+      if(this.request.type==0){
+        this.refresh()
+        return
+      }
+      let dom=event.currentTarget
+      let domWid=dom.clientWidth
+      let domHei=dom.clientHeight
+
+      let originWid=dom.width
+      let originHei=dom.height
+
+      let rect = dom.getBoundingClientRect();
+
+      let x=event.clientX-dom.offsetLeft
+      let y=event.clientY-dom.offsetTop
+
+      let style = window.getComputedStyle(document.body,null)
+
+      this.points.push({
+        x: x*100.0/domWid,
+        y: y*100.0/domHei
+      })
+      let count=Math.max(1,this.request.count)
+      if(this.points.length>=count){
+        let str=''
+        for(let i=0;i<count;i++){
+          if(this.request.type==1){
+            str+=';'+this.points[i].x.toFixed(2)
+          }else if(this.request.type==2){
+            str+=';'+this.points[i].x.toFixed(2)
+          }else if(this.request.type==3){
+            str+=';'+this.points[i].x.toFixed(2)+','+this.points[i].y.toFixed(2)
+          }else if(this.request.type==4){
+            str+=';'+this.points[i].x.toFixed(2)+','+this.points[i].y.toFixed(2)
+          }
+        }
+        if(str!=''){
+          str=str.substring(1)
+        }
+        this.result=str
+        this.submit()
+      }
+    },
   },
   computed: {
 
