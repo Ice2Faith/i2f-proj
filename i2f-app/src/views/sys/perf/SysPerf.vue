95dcb0bb723f6645eb7db39cf018ab1e4c9c771a
Ice2Faith <Ugex_Savelar@163.com>
2023-08-26 23:37:59 +0800

    perf page

---
+++ i2f-app/src/views/sys/perf/SysPerf.vue
@@ -0,0 +1,27 @@
+<template>
+  <div >
+    perf
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    this.$emit('nav',{
+      enable: true,
+      title: '性能监控',
+      rightText: null,
+      rightIcon: 'search',
+      rightClick: this.search
+    })
+  },
+  methods: {
+    search(){
+      this.$message.noticeSuccess('搜索')
+    }
+  }
+}
+</script>
+<style>
+
+
+</style>
